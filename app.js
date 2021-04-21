270c61f14e5b8c6fa7b9c7e55e711d989301fd42
pokion <pokion@outlook.com>
2021-04-21 16:50:19 +0200

    get all champions
    -funkcja to pobierania wszystkich postaci z mobafire

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 /*mobafire.js is only for download page content*/
 const moba  = require('./mobafire/mobafire.js')
 
-moba.getAllChampions()
+moba.getAllChampions((champions)=>{
+	console.log(champions,'form app.js')
+})
 
 const port = process.env.PORT || 3030;
 
--- mobafire/mobafire.js
+++ mobafire/mobafire.js
@@ -1,20 +1,29 @@
-const http = require('http')
 const request = require('request');
 
 module.exports = {
 	/*getAllChampions this function returns all champions in mobafire*/
-	getAllChampions: function(){
+	getAllChampions: function(callback){
 		/*connect to mobafire*/
 		request('https://www.mobafire.com/', (err,res,body)=>{
-			console.error('err',err);
 			if(!err){
-				let arrayChampions;
+				let arrayChampions = [];/*array contain all champions and path*/
 				let splitDivChampions = body.split('<div class="footer-links">')[1].split('</div>')[0].split('\n');/*split only div with all champions*/
 				splitDivChampions.forEach((elem)=>{
-					let patternPath = new RegExp("(?<=\")(.*?)(?=\<)","g");
-					let path = elem.match(patternPath);
-					for(const item of path) console.log(item)
+					if(elem != ''){
+						let patternPath = new RegExp("(?<=\")(.*?)(?=\<)","g");
+						let path = elem.match(patternPath);
+						let splitNameAndPath = path[0].split('">');
+						arrayChampions.push({
+							path: splitNameAndPath[0],
+							name: splitNameAndPath[1]
+						})
+					}
 				})
+				if(!callback){
+					console.error('you must pass callback')
+				}else if(typeof callback == "function"){
+					callback(arrayChampions)
+				}
 			}else{
 				throw err
 			}
