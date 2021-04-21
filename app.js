c0651da458a073ef00a6a47140e8610d3a27195f
pokion <pokion@outlook.com>
2021-04-21 22:02:02 +0200

    create functions grab

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express();
 /*mobafire.js is only for download page content*/
 const moba  = require('./mobafire/mobafire.js')
 
-moba.getAllChampions((champions)=>{
+/*moba.getAllChampions((champions)=>{
 	console.log(champions,'form app.js')
+})*/
+/*moba.getBuildPath('/league-of-legends/taliyah-guide',(body)=>{
+	console.log(body)
+})*/
+moba.getguide('/league-of-legends/build/dumb-taliyah-support-guide-586468',(body)=>{
+	console.log(body)
 })
 
 const port = process.env.PORT || 3030;
--- mobafire/mobafire.js
+++ mobafire/mobafire.js
@@ -1,32 +1,63 @@
 const request = require('request');
+const cheerio = require('cheerio');
 
 module.exports = {
-	/*getAllChampions this function returns all champions in mobafire*/
-	getAllChampions: function(callback){
-		/*connect to mobafire*/
-		request('https://www.mobafire.com/', (err,res,body)=>{
+	connect: function(callback,path){
+		let pathToUrl = path || '';
+		request('https://www.mobafire.com'+path,(err,res,body)=>{
 			if(!err){
-				let arrayChampions = [];/*array contain all champions and path*/
-				let splitDivChampions = body.split('<div class="footer-links">')[1].split('</div>')[0].split('\n');/*split only div with all champions*/
-				splitDivChampions.forEach((elem)=>{
-					if(elem != ''){
-						let patternPath = new RegExp("(?<=\")(.*?)(?=\<)","g");
-						let path = elem.match(patternPath);
-						let splitNameAndPath = path[0].split('">');
-						arrayChampions.push({
-							path: splitNameAndPath[0],
-							name: splitNameAndPath[1]
-						})
-					}
-				})
-				if(!callback){
-					console.error('you must pass callback')
-				}else if(typeof callback == "function"){
-					callback(arrayChampions)
-				}
-			}else{
+				callback(body);
+			}else if(err){
 				throw err
 			}
-		});
+		})
+	},
+	/*getAllChampions this function returns all champions in mobafire*/
+	getAllChampions: function(callback){
+		this.connect((body)=>{
+			let arrayChampions = [];/*array contain all champions and path*/
+			let splitDivChampions = body.split('<div class="footer-links">')[1].split('</div>')[0].split('\n');/*split only div with all champions*/
+			splitDivChampions.forEach((elem)=>{
+				if(elem != ''){
+					let patternPath = new RegExp("(?<=\")(.*?)(?=\<)","g");/*pattern for extract only link and champion*/
+					let path = elem.match(patternPath);
+					let splitNameAndPath = path[0].split('">');
+					arrayChampions.push({
+						path: splitNameAndPath[0],
+						name: splitNameAndPath[1]
+					})
+				}
+			})
+			if(!callback){
+				console.error('you must pass callback')
+			}else if(typeof callback == "function"){
+				callback(arrayChampions)/*return champions to rift xD*/
+			}
+		})
+		
+	},
+	getBuildPath: function(path,callback){
+		this.connect((body)=>{
+			let $ = cheerio.load(body)
+			let elemA = $('.browse-list>a')
+			let arr = [];
+			for(let i=0;i<elemA.length;i++){
+				let href = elemA[i].attribs.href
+				let Role = $(elemA[i]).find('div.browse-list__item__pic')[0].attribs.class.split(' ');
+				arr.push({
+					href,
+					role: Role.length > 1 ? Role[1] : null
+				})
+			}
+
+			callback(arr)
+		},path)
+	},
+	getguide: function(path,callback){
+		this.connect(body =>{
+			let $ = cheerio.load(body);
+			let runes = $('.new-runes__primary')
+			callback(runes)
+		},path)
 	}
 }
